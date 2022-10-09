79e274a3a963523db6605cc72e0603b175dcfe08
Y K <97624711+yxk9640@users.noreply.github.com>
2022-10-09 18:45:28 -0500

    Final update
    Blcok method updated with serialization

--- CloudComputing/project2/src/main/java/Add.java
+++ CloudComputing/project2/src/main/java/Add.java
@@ -9,20 +9,20 @@ import org.apache.hadoop.mapreduce.lib.input.*;
 import org.apache.hadoop.mapreduce.lib.output.*;
 
 
-
 class Triple implements Writable {
     public int i;
     public int j;
     public double value;
-
+	
     Triple () {}
-
+	
     Triple ( int i, int j, double v ) { this.i = i; this.j = j; value = v; }
-
+	
     public void write ( DataOutput out ) throws IOException {
         out.writeInt(i);
         out.writeInt(j);
         out.writeDouble(value);
+
     }
 
     public void readFields ( DataInput in ) throws IOException {
@@ -41,20 +41,30 @@ class Block implements Writable {
     final static int rows = 100;
     final static int columns = 100;
     public double[][] data;
-    Pair p = new Pair();
-    Triple t = new Triple();
+    Pair pair = new Pair();
+    Triple triple = new Triple();
 
     Block () {
         data = new double[rows][columns];
     }
 
-    public void write ( DataOutput out ) throws IOException {
+    public void write ( DataOutput out ) throws IOException {//serialisation
+        Block inBlock = new Block();
 
-        out.writeDouble(data[p.i][p.j]);
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < columns; j++){
+                out.writeDouble(data[i][j]);
+            }
+        }
     }
 
-    public void readFields ( DataInput in ) throws IOException {
-        data[t.i][t.j] = in.readDouble();
+    public void readFields ( DataInput in ) throws IOException {//deserialisation
+        Block outBlock = new Block();
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < columns; j++){
+                data[i][j] = in.readDouble();
+            }
+        }
     }
 
     @Override
@@ -72,11 +82,11 @@ class Block implements Writable {
 class Pair implements WritableComparable<Pair> {
     public int i;
     public int j;
-
+	
     Pair () {}
-
+	
     Pair ( int i, int j ) { this.i = i; this.j = j; }
-
+	
     public void write ( DataOutput out ) throws IOException {
         out.writeInt(i);
         out.writeInt(j);
@@ -87,10 +97,9 @@ class Pair implements WritableComparable<Pair> {
         j = in.readInt();
     }
 
-
     @Override
     public int compareTo ( Pair o ) {
-        return i == o.i? j-o.j : i-o.i;// check for logical errors
+        return this.i == o.i? j-o.j : this.i-o.i; //Logic
     }
 
     @Override
@@ -99,16 +108,14 @@ class Pair implements WritableComparable<Pair> {
     }
 }
 
-
-
-
 public class Add extends Configured implements Tool {
     final static int rows = Block.rows;
     final static int columns = Block.columns;
 
     /* ... */
-//N matrix mapper
-   public static class SparseToBlockMapper extends Mapper<Object, Text, Pair, Triple>{
+
+
+    public static class SparseToBlockMapper extends Mapper<Object, Text, Pair, Triple>{
         @Override
         public void map(Object key, Text value,Context context) throws IOException, InterruptedException {
             Scanner inputM = new Scanner(value.toString());
@@ -129,60 +136,55 @@ public class Add extends Configured implements Tool {
     public static class SparseToBlockReducer extends Reducer<Pair, Triple, Pair,Block>{
         @Override
         public void reduce(Pair key, Iterable<Triple> values, Context context) throws IOException, InterruptedException {
-            Block dm = new Block();
+            Block SparsetoBlock = new Block();
             for ( Triple v: values){
-                   dm.data[v.i][v.j]= v.value;
+                SparsetoBlock.data[v.i][v.j]= v.value;
             }
-            context.write(key, dm);
-
+            context.write(key, SparsetoBlock); //sequence
         }
     }
 
-
-
-    public static class AddMMapper extends Mapper<Object, Text, Pair, Block>{
+      public static class AddMMapper extends Mapper<Pair, Block,Pair, Block>{
         @Override
-        public void map(Object key, Text value,Context context) throws IOException, InterruptedException {
-            context.write(new Pair(), new Block());
+        public void map(Pair key, Block value,Context context) throws IOException, InterruptedException {
+                context.write(key,value);
         }
     }
 
-    public static class AddNMapper extends Mapper<Object, Text, Pair, Block>{
+    public static class AddNMapper extends Mapper<Pair, Block, Pair, Block>{
         @Override
-        public void map(Object key, Text value,Context context) throws IOException, InterruptedException {
-            context.write(new Pair(),new Block());
+        public void map(Pair key, Block value, Context context) throws IOException, InterruptedException {
+            context.write(key,value);
            }
     }
 
     public static class ResultReducer extends Reducer<Pair,Block,Pair,Block>{
         @Override
-        public void reduce(Pair key, Iterable<Block> values, Context context)
-                throws IOException, InterruptedException {
-            Block blockM = new Block();
+        protected void reduce(Pair key, Iterable<Block> values, Context context) throws IOException, InterruptedException {
 
-            Block blockN = new Block();
+            Block resBlock = new Block();
+//            res.data[0][0]=0;
 
-            for (Block b:values){
-                blockM.data = b.data;
+            for(Block b: values){
+                for (int i = 0; i < b.rows; i++){
+                    for (int j = 0; j < b.columns; j++){
+                        resBlock.data[i][j] += b.data[i][j];
+                    }
+                }
             }
-
-//            for (int i = 0; i < blockM.data.length; i++){
-//                for (int j = 0; j < blockN.data.length; j++){
-//                        blockM.data[i][j] = blockN.data[i][j];
-//                }
-//            }
-            context.write(key,blockM);
+            context.write(key, resBlock);
         }
     }
-
 
 
     @Override
     public int run ( String[] args ) throws Exception {
         /* ... */
+
         //job description goes here check for simple and join programs
-        //Mapper job for matrix M
-        Job jobM = Job.getInstance();
+        Configuration conf = getConf();
+        //Sparse to Dense for matrix M
+        Job jobM = Job.getInstance(conf,"JobM");
         jobM.setJobName("SparseBlockM");
         jobM.setJarByClass(Add.class);
 
@@ -196,12 +198,14 @@ public class Add extends Configured implements Tool {
         jobM.setReducerClass(SparseToBlockReducer.class);
 
         jobM.setInputFormatClass(TextInputFormat.class);
-        jobM.setOutputFormatClass(TextOutputFormat.class);
+        jobM.setOutputFormatClass(SequenceFileOutputFormat.class);// used for sequence
+//        jobM.setOutputFormatClass(TextOutputFormat.class);// used for standard
         FileInputFormat.setInputPaths(jobM, new Path(args[0]));
         FileOutputFormat.setOutputPath(jobM, new Path(args[2]));
         jobM.waitForCompletion(true);
 
-        Job jobN = Job.getInstance();
+        //Sparse to Block matrix-N Block
+        Job jobN = Job.getInstance(conf, "jobN");
         jobN.setJobName("SparseBlockN");
         jobN.setJarByClass(Add.class);
 
@@ -215,32 +219,34 @@ public class Add extends Configured implements Tool {
         jobN.setReducerClass(SparseToBlockReducer.class);
 
         jobN.setInputFormatClass(TextInputFormat.class);
-        jobN.setOutputFormatClass(TextOutputFormat.class);
+        jobN.setOutputFormatClass(SequenceFileOutputFormat.class);//sequence mode
+//        jobN.setOutputFormatClass(TextOutputFormat.class);// used for std mode
         FileInputFormat.setInputPaths(jobN, new Path(args[1]));
         FileOutputFormat.setOutputPath(jobN, new Path(args[3]));
         jobN.waitForCompletion(true) ;
 
 
-
-        //Mapper job for matrix Block
-        Job jobResult = Job.getInstance();
+//        Addition
+        Job jobResult = Job.getInstance(conf,"jobResult");
         jobResult.setJobName("Addition");
         jobResult.setJarByClass(Add.class);
 
-        jobResult.setOutputKeyClass(Pair.class); //Reducer
-        jobResult.setOutputValueClass(Block.class); //Reducer
+        jobResult.setOutputKeyClass(Pair.class); //Reducer-Key
+        jobResult.setOutputValueClass(Block.class); //Reducer-Value
 
         jobResult.setMapOutputKeyClass(Pair.class);
         jobResult.setMapOutputValueClass(Block.class);
 
         jobResult.setReducerClass(ResultReducer.class);
         jobResult.setOutputFormatClass(TextOutputFormat.class);
-        MultipleInputs.addInputPath(jobResult,new Path("left_tmp/part-r-00000"),TextInputFormat.class,AddMMapper.class);
-        MultipleInputs.addInputPath(jobResult,new Path("right_tmp/part-r-00000"),TextInputFormat.class,AddNMapper.class);
+        MultipleInputs.addInputPath(jobResult,new Path(args[2]+"/part-r-00000"), SequenceFileInputFormat.class,AddMMapper.class);// seq
+//        MultipleInputs.addInputPath(jobResult,new Path("left_tmp/part-r-00000"), TextInputFormat.class,AddMMapper.class);
+        MultipleInputs.addInputPath(jobResult,new Path(args[3]+"/part-r-00000"), SequenceFileInputFormat.class,AddNMapper.class);//seq
+//        MultipleInputs.addInputPath(jobResult,new Path("right_tmp/part-r-00000"), TextInputFormat.class,AddNMapper.class);
         FileOutputFormat.setOutputPath(jobResult,new Path(args[4]));
         jobResult.waitForCompletion(true);
-        return 0;
 
+        return 0;
     }
 
     public static void main ( String[] args ) throws Exception {
