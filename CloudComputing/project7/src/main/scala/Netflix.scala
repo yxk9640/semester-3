3fc29b68b7d44e4beeb683d2d5fdb09ad788eb9d
Y K <97624711+yxk9640@users.noreply.github.com>
2022-11-16 10:56:23 -0600

    Cloud project 7 completed Local mode is good should run in distributed mode

--- CloudComputing/project7/src/main/scala/Netflix.scala
+++ CloudComputing/project7/src/main/scala/Netflix.scala
@@ -1,5 +1,7 @@
 import org.apache.spark.{SparkContext, _}
 import org.apache.spark.sql._
+import org.apache.spark.sql.functions
+
 
 object Netflix {
 
@@ -9,25 +11,24 @@ object Netflix {
     val spark = SparkSession.builder().config(conf).getOrCreate()
     import spark.implicits._
 /* ... */
-
-
     val line = sparkContext.textFile(args(0)).filter(line => !line.contains(":"))
-
     val dataFrame = line.map(
       line => {
         val in = line.split(",")
         (in(0).toInt , in(1).toDouble) }).toDF("user","ratings")
 
+
 //    dataFrame.printSchema()
+    //generate avg of ratings
     dataFrame.createOrReplaceTempView("review")
-//      SELECT user, sum(ratings) from dataFrame group by user asc
-//    dataFrame.select($"user",$"ratings").f
-    val avgRating = spark.sql(" SELECT  user, round(AVG(ratings),1) as rating FROM review GROUP BY user ")
-    avgRating.show()
+    val avgRating = spark.sql(" SELECT  user, floor((SUM(ratings)/COUNT(ratings))*10)/10 as rating FROM review GROUP BY user ")
+//    avgRating.show()
     avgRating.createOrReplaceTempView("reviewFreq")
 
+    //count ratings
     val res = spark.sql("SELECT rating,COUNT(rating) as count FROM reviewFreq GROUP BY rating ORDER BY rating ASC ")
-    res.show()
+//    res.show()
+    res.collect.foreach(x=> println(x.get(0)+" "+x.get(1)))
 
     //    val freqRating = spark.sql()
 
